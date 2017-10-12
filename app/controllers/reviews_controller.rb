d5f551bef8861b9cbccef427c6c68ff1572d331a
Rahul Ramesh <rahul.ramesh888@gmail.com>
2017-10-12 19:32:15 -0400

    Delete button implemented. But, delete route not working. Need to fix link_to for delete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_review, only: [:show, :edit, :update]
   before_action :authorize
 
   # GET /reviews/1
